d83804f22c4474315fe9ac7f32573e27f7cc48bc
Benceroni <bell.spencer@icloud.com>
2025-09-09 16:20:14 -0600

    finished JS examples

--- javascriptPractice.js
+++ javascriptPractice.js
@@ -1,6 +1,40 @@
-const DAYS = 6;
+// const DAYS = 6;
 const LIMIT = 30;
 let studentReport = [11,42,33,64,29,37,44];
 
-for (let i = 0; i = studentReport.length)
-console.log()
+for (let i = 0; i < studentReport.length; i+=1){
+    if (studentReport[i] <= LIMIT){
+        console.log(studentReport[i])
+    }
+}
+
+let i = 0;
+while (i< studentReport.length){
+    if (studentReport[i] < LIMIT){
+        console.log(studentReport[i])
+        console.log(i)
+        
+    }
+    i += 1;
+}
+
+studentReport.forEach(Number => {
+    if (Number < LIMIT){
+        console.log(Number)
+    }
+});
+
+for (number in studentReport){
+    if (studentReport[number] < LIMIT){
+        console.log(studentReport[number])
+    }
+
+}
+
+const DAYS = 6;
+const options = { weekday: 'long'};
+
+for (i = 0; i<DAYS; i+=1){
+    let nextDay = new Date().setDate(new Date().getDate() + i)
+    console.log(new Intl.DateTimeFormat("en-US",options).format(nextDay))
+}
