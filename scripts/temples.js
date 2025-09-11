313d04dcf95b76f83d779a4f0c422cca4122addb
Benceroni <bell.spencer@icloud.com>
2025-09-10 20:25:02 -0600

    temples page

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,10 @@
 document.getElementById("currentyear").innerHTML = "©" + new Date().getFullYear() + "- Spencer Bell - Idaho, United States"
 document.getElementById("lastModified").innerHTML = "last Modification: " + document.lastModified
+
+const navId = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', function() {
+    navId.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
