552305e9ce039f06702c05cba483ad85abdd84f9
Benceroni <bell.spencer@icloud.com>
2025-09-20 21:05:32 -0600

    Pushing to show her :)

--- scripts/places.js
+++ scripts/places.js
@@ -1,2 +1,21 @@
 document.getElementById("currentyear").innerHTML = "©" + new Date().getFullYear() + "- Spencer Bell - Idaho, United States"
 document.getElementById("lastModified").innerHTML = "last Modification: " + document.lastModified
+
+let currentTemperature = 30;
+let windSpeed = 14;
+let conditions = "Overcast";
+
+document.querySelector("#temperature").innerHTML = `${currentTemperature}°F`;
+document.querySelector("#wind").innerHTML= `${windSpeed} MPH`;
+document.querySelector("#conditions").innerHTML = conditions;
+document.querySelector("#windchill").innerHTML = calculateWindChill();
+
+function calculateWindChill(){
+    if (currentTemperature > 50 || windSpeed <3 ){
+        return "N/A"
+    }
+
+    let _windchill = Math.round(35.74 + .6215*currentTemperature - 35.75* Math.pow(windSpeed,.16) + .4275* currentTemperature * Math.pow(windSpeed,.16));
+
+    return `${_windchill}°F`;
+}
