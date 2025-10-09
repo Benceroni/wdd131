41d58b083b63b6b84d0cbde608ff642abaa19013
Benceroni <bell.spencer@icloud.com>
2025-10-08 22:34:57 -0600

    IT WORKED MY FIRST TRY  LET'S GOOOOOOOOOOOOOOO

---
+++ project/scripts/love4haytEntries.js
@@ -0,0 +1,73 @@
+const entries =[
+    {
+        date: "2024-07-21",
+        imageReference: "",
+        text: "work on the rav4 is slow but going"
+    },
+    {
+        date: "2024-08-12",
+        imageReference: "",
+        text: "I got the rav4 rebuilt but it's still broken"
+    },
+    {
+        date: "2024-09-25",
+        imageReference: "",
+        text: "The rav4 runs again"
+    },
+    {
+        date: "2025-02-21",
+        imageReference: "",
+        text: "The Rav4 died again. Rip"
+    }
+]
+
+const monthNames = ["January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+function getMonthStringFromFullDate(_fullDate){
+    const monthAsInteger = parseInt(_fullDate.substring(5,7));
+    console.log(monthAsInteger)
+    return monthNames[monthAsInteger-1]
+}
+
+// console.log(getMonthStringFromFullDate("0000-08-00"));
+
+function assembleFormattedDate(_date){
+    return `${_date.substring(8,10)} ${getMonthStringFromFullDate(_date)} ${_date.substring(0,4)}`
+}
+
+console.log(assembleFormattedDate("2018-12-10"));
+ 
+entries.forEach(entry => 
+    document.querySelector("#entries").innerHTML +=
+    assembleEntry(assembleFormattedDate(entry.date),entry.ImageReference,entry.text));
+
+
+
+function assembleEntry(_date,_imageReference,_text){
+    return `<H3>${_date}</H3><img src ="images/${_imageReference}"><p>${_text}</p>`
+}
+
+
+
+//template
+// ,
+//     {
+//         date: "",
+//         ImageReference: "",
+//         text: ""
+//     }
+
+// date is formatted yyyy-mm-dd
+// image referecne is just the file name within "images/image_name.webp"
